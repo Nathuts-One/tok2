62acee5ee554ebadeb93091fa49f7cfe83ec8d3d
Nathuts-One <renato.f.santos@gmail.com>
2026-04-07 19:32:58 -0300

    feat: add figma.config.ts

---
+++ figma.config.ts
@@ -0,0 +1,9 @@
+import { CodeConnectConfig } from '@figma/code-connect';
+
+const config: CodeConnectConfig = {
+  figmaFile: 'https://www.figma.com/design/uKkZkyJQUzKE33GgsZCd7s/Teste-Figma-mcp-v2',
+  include: ['src/**/*.figma.ts'],
+  parser: 'custom',
+};
+
+export default config;
