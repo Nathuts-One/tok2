96c6572765d6b7f568fc4a6e65571dc7c82e08cf
Nathuts-One <renato.f.santos@gmail.com>
2026-04-07 17:48:41 -0300

    chore: upgrade to Style Dictionary v4 + DTCG format

--- sd.config.js
+++ sd.config.js
@@ -1,23 +1,29 @@
-const StyleDictionary = require('style-dictionary');
+import StyleDictionary from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
 
-module.exports = {
+register(StyleDictionary);
+
+const sd = new StyleDictionary({
   source: ['tokens/**/*.json'],
+  preprocessors: ['tokens-studio'],
   platforms: {
     css: {
-      transformGroup: 'css',
+      transformGroup: 'tokens-studio',
       prefix: 'ds',
       buildPath: 'dist/css/',
       files: [{ destination: 'variables.css', format: 'css/variables' }]
     },
     js: {
-      transformGroup: 'js',
+      transformGroup: 'tokens-studio',
       buildPath: 'dist/js/',
       files: [{ destination: 'tokens.js', format: 'javascript/es6' }]
     },
     ts: {
-      transformGroup: 'js',
+      transformGroup: 'tokens-studio',
       buildPath: 'dist/ts/',
       files: [{ destination: 'tokens.d.ts', format: 'typescript/es6-declarations' }]
     }
   }
-};
+});
+
+await sd.buildAllPlatforms();
